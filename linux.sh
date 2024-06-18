48b1d766fc4a993a5a16be26dbe93e75d3598997
nnquangpro <nnquangpro>
2024-06-18 04:39:43 +0000

    PHP Binary KernelV1-BootloaderV1.1-PHP8.3

---
+++ linux.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+ARCH=$(uname -m)
+OS=$(grep '^ID=' /etc/os-release | cut -d= -f2)
+VERSION=$(grep 'VERSION_ID=' /etc/os-release | cut -d= -f2 | tr -d '"')
+
+case $ARCH in
+#    aarch64)
+#        ARCH="arm64"
+#        ;;
+#    armv7l)
+#        ARCH="armv7"
+#        ;;
+    x86_64)
+        ARCH="x86_64"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+URL=""
+case $OS in
+    debian)
+        URL="https://raw.githubusercontent.com/nnquangpro/PHP-Binary/main/packages/debian-${VERSION}/${ARCH}/phpb"
+        ;;
+    ubuntu)
+        URL="https://raw.githubusercontent.com/nnquangpro/PHP-Binary/main/packages/ubuntu-${VERSION}/${ARCH}/phpb"
+        ;;
+    rockylinux)
+        URL="https://raw.githubusercontent.com/nnquangpro/PHP-Binary/main/packages/rockylinux-${VERSION}/${ARCH}/phpb"
+        ;;
+    *)
+        echo "Unsupported OS: $OS"
+        exit 1
+        ;;
+esac
+
+curl -L -o $HOME/phpb $URL
+chmod +x $HOME/phpb
+
+echo "PHP binary installed successfully"
--- termux.sh
+++ termux.sh
@@ -1,7 +1,28 @@
-arch=$(uname -m)
-Bindir="$PREFIX/bin"
-curl -o "$Bindir/phpb" "https://raw.githubusercontent.com/nnquangpro/PHP-Binary/main/packages/termux/$arch/phpb"
-chmod +x "$Bindir/phpb"
-echo "-------------------------"
-echo "Đã cài thành công PHP Binary"
-echo
+#!/bin/bash
+
+ARCH=$(uname -m)
+
+case $ARCH in
+    aarch64)
+        URL="https://raw.githubusercontent.com/nnquangpro/PHP-Binary/main/packages/termux/aarch64/phpb"
+        ;;
+    armv7l)
+        URL="https://raw.githubusercontent.com/nnquangpro/PHP-Binary/main/packages/termux/arm/phpb"
+        ;;
+    i686)
+        URL="https://raw.githubusercontent.com/nnquangpro/PHP-Binary/main/packages/termux/i686/phpb"
+        ;;
+    x86_64)
+        URL="https://raw.githubusercontent.com/nnquangpro/PHP-Binary/main/packages/termux/x86_64/phpb"
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
+
+curl -L -o /data/data/com.termux/files/usr/bin/phpb $URL
+chmod +x /data/data/com.termux/files/usr/bin/phpb
+
+echo "PHPBinary installed successfully"
+
